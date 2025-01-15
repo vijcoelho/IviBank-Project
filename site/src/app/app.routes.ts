66bcf85054f1f1ad000116c531f1e29f57a9f1aa
Vitor Coelho <130684606+vijcoelho@users.noreply.github.com>
2025-01-15 01:32:42 -0300

    screen signup create
    need to be a better signup screen, and i will put api on this to create an user

--- site/src/app/app.routes.ts
+++ site/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './component/main/main.component';
 import { NgModule } from '@angular/core';
 import { AboutComponent } from './component/about/about.component';
+import { SignupComponent } from './component/signup/signup.component';
 
 export const routes: Routes = [
   { path: '', component: MainComponent, pathMatch: 'full'},
   { path: 'main', component: MainComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'signup', component: SignupComponent}
 ];
 
 @NgModule({
---
+++ site/src/app/component/signup/signup.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { TopbarComponent } from "../topbar/topbar.component";
+
+@Component({
+  selector: 'app-signup',
+  imports: [TopbarComponent],
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.css'
+})
+export class SignupComponent {
+
+}
