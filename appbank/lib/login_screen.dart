0490f0658aa6383511217dfaaf9882204de3942b
Vitor Coelho <130684606+vijcoelho@users.noreply.github.com>
2025-02-24 18:02:14 -0300

    commit firts login screen

--- appbank/lib/login_screen.dart
+++ appbank/lib/login_screen.dart
@@ -1,73 +1,143 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class BackgroundPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final Paint paint = Paint();
+
+    paint.color = Colors.pink.shade100.withOpacity(0.9);
+    canvas.drawCircle(Offset(size.width * 0.1, size.height * 0.9), 180, paint);
+
+    paint.color = Colors.pink.shade100.withOpacity(0.5);
+    canvas.drawCircle(Offset(size.width * 0.4, size.height * 0.8), 120, paint);
+
+    paint.color = Colors.pink.shade200.withOpacity(0.8);
+    canvas.drawCircle(Offset(size.width * 0.7, size.height * 0.9), 200, paint);
+
+    paint.color = Colors.pink.shade200.withOpacity(0.4);
+    canvas.drawCircle(Offset(size.width * 0.9, size.height * 0.82), 200, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return false;
+  }
+}
 
 class Login extends StatelessWidget {
-  const Login({super.key});
+  Login({super.key});
+
+  final TextEditingController _controllerEmail = TextEditingController();
+  final TextEditingController _controllerPassword = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(40),
-          child: Container(
-            width: 300,
-            height: 400,
-            padding: EdgeInsets.all(20),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(10),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.white.withOpacity(0.2),
-                  spreadRadius: 3,
-                  blurRadius: 10,
+      backgroundColor: Colors.white,
+      body: CustomPaint(
+        painter: BackgroundPainter(),
+        child: Center(
+          child: Column(
+            children: [
+              Image(
+                height: 125,
+                image: AssetImage('ivibank_logo.png')
+              ),
+              Padding(
+                padding: const EdgeInsets.all(50.0),
+                child: Text(
+                  "Login",
+                  style: GoogleFonts.montserrat(
+                    fontSize: 40, 
+                    fontWeight: FontWeight.bold
+                  ),
                 ),
-              ],
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(
-                  width: 250,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 50),
+                child: SizedBox(
+                  width: 350,
                   child: TextField(
+                    controller: _controllerEmail,
                     keyboardType: TextInputType.emailAddress,
                     decoration: InputDecoration(
                       hintText: "Email",
-                      contentPadding: EdgeInsets.symmetric(
-                        vertical: 10, horizontal: 12), 
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.grey, width: 1.5),
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                      hintStyle: TextStyle(
-                        fontWeight: FontWeight.w300,
-                        color: Colors.black45,
-                      ),
+                      prefixIcon: Icon(Icons.email, color: Colors.black),
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.pinkAccent)
+                      )
                     ),
-                  ),
+                  )
                 ),
-                SizedBox(
-                  width: 250,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 100),
+                child: SizedBox(
+                  width: 350,
                   child: TextField(
+                    controller: _controllerPassword,
                     keyboardType: TextInputType.text,
                     decoration: InputDecoration(
                       hintText: "Password",
-                      contentPadding: EdgeInsets.symmetric(
-                        vertical: 10, horizontal: 12), 
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.grey, width: 1.5),
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                      hintStyle: TextStyle(
-                        fontWeight: FontWeight.w300,
-                        color: Colors.black45,
-                      ),
+                      prefixIcon: Icon(Icons.password, color: Colors.black),
+                      focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.pinkAccent)
+                      )
+                    ),
+                  )
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 125),
+                child: ElevatedButton(
+                  onPressed: () {
+                    print("button login test");
+                  }, 
+                  child: Text (
+                    "login",
+                    style: GoogleFonts.montserrat(
+                      fontSize: 15, 
+                      fontWeight: FontWeight.w500
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color.fromARGB(255, 255, 115, 162),
+                    foregroundColor: Colors.black,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30)
+                    )
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: GestureDetector(
+                  onTap: () {
+                    print("create account button");
+                  },
+                  child: Text(
+                    "create account",
+                    style: GoogleFonts.montserrat(
+                        fontSize: 15, 
+                        fontWeight: FontWeight.w500
                     ),
                   ),
                 ),
-              ],
-            ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  print("forgot password button");
+                },
+                child: Text(
+                  "forgot password",
+                  style: GoogleFonts.montserrat(
+                    fontSize: 15, 
+                    fontWeight: FontWeight.w500
+                  ),
+                ),
+              )
+            ],
           ),
         ),
       ),
